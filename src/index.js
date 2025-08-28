456cb9fd9cf2e4d3faeacc68d7d9314d04378ee7
AJ-Holzer <axel123.holzer@gmail.com>
2025-08-28 13:00:44 +0200

    Add dotenv package for environment variables

--- src/index.js
+++ src/index.js
@@ -1,17 +1,28 @@
 require("dotenv").config();
 
-const { Client, IntentsBitField } = require('discord.js');
+// Variables
+const token = process.env.BOT_TOKEN;
+
+const { Client, IntentsBitField } = require("discord.js");
 
 const client = new Client({
-    intents: [
-        IntentsBitField.Flags.Guilds,
-        IntentsBitField.Flags.GuildMembers,
-        IntentsBitField.Flags.GuildMessages,
-        IntentsBitField.Flags.MessageContent,
-    ],
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
 });
 
+// Log if bot is online
+client.on("clientReady", (cli) => {
+  console.log(`✅ ${cli.user.username} is online.`);
+});
 
-// Variables
-const token = process.env.BOT_TOKEN
-const guildID = process.env.GUILD_ID
+// Log messages entered in the dc chat for testing purpose
+client.on("messageCreate", (msg) => {
+  console.log(msg);
+});
+
+// Login the bot
+client.login(token);
